ddbdc15e0e093a192eb4d48f7638acd17cde614d
prasad-takale-eminds <prasad.takale@eminds.ai>
2023-06-15 16:12:21 +0530

    Code Migrated from Beckn Repo

--- beckn-sandbox/src/schemas/callbacks/request.callback.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-import { becknContextSchema } from "../becknContext.schema";
-
-export const requestCallbackSchema = z.object({
-    context: becknContextSchema,
-    message: z.any(),
-});
-
-export type RequestCallbackDataType = z.infer<typeof requestCallbackSchema>;
